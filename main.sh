54561963004ef61b757e414bcf75fe5de6f3d0ed
Liam Xu <jx17@illinois.edu>
2022-10-10 16:42:13 -0500

    updated main.sh

--- main.sh
+++ main.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-# usage: main.sh <the desired number of processes>
+# usage: main.sh <starting rank> <ending rank>
 START=$1
 END=$2
 for (( i=$START; i<$END; i++ ))
 do
-   python main.py --rank $i --num_proc 1
+   python main.py --rank $i
 done
