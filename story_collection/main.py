860247ca313f58126b50842cfe1e3f9843bd70fb
Liam Xu <jx17@illinois.edu>
2022-10-16 04:17:29 -0500

    updated story_collection

--- story_collection/main.py
+++ story_collection/main.py
@@ -9,58 +9,144 @@ from selenium.webdriver.chrome.options import Options  # for suppressing the bro
 from selenium.common.exceptions import WebDriverException
 
 
-def main(source: str = 'main'):
-    href_name = source.split('/')[-1]
-    root_url = "https://ground.news"
+def main(args):
+    # set up driver
+    if args.headless:
+        chrome_options = Options()
+        chrome_options.add_argument("--headless")
+        driver = webdriver.Chrome(options=chrome_options)
+    else:
+        driver = webdriver.Chrome()
+    driver.get('https://ground.news')
+    with open('credentials.json', 'r', encoding='utf-8') as f:
+        creds = json.load(f)
+    username = creds['username']
+    password = creds['password']
+    login(driver, username, password)
 
-    hrefs = get_hrefs(source)
-    print('length of found hrefs:', len(hrefs))
+    # set up data folders
+    if not os.path.exists(f'story_collection/{args.tag}_logs/'):
+        os.mkdir(f'story_collection/{args.tag}_logs/')
+    if not os.path.exists(f'story_collection/{args.tag}_interest/'):
+        os.mkdir(f'story_collection/{args.tag}_interest/')
 
-    result = {}
-    for idx, href in enumerate(hrefs):
-        full_url = root_url + href
-        story_data = get_one_story(full_url)
+    # do the actual collection
+    if args.source == 'topic_list':
+        # Initialize the logs.
+        logs = ['In Progress']
+        with open(f'story_collection/{args.tag}_logs/logs.json', 'w', encoding='utf-8') as f:
+            json.dump(logs, f, ensure_ascii=False, indent=4)
+
+        # Get topic_list.
+        with open('topic_collection/topic_list.json', 'r') as f:
+            topic_list = json.load(f)
+        topic_list = list(sorted(topic_list.items()))
+
+        # Go through the segment and log each the status of each topic.
+        for topic, topic_href in topic_list:
+            try:
+                # For each href in the topic_list, get stories for that href.
+                tic = time.time()
+                source, story_num, article_num = get_all_story_per_href(driver, topic_href, args.tag)
+                toc = time.time()
+                logs.append({
+                    'topic': topic,
+                    'status': 'Successful',
+                    'time': toc - tic,
+                    'source': source,
+                    'story_num': story_num,
+                    'article_num': article_num
+                })
+            except (BaseException, WebDriverException) as e:
+                logs.append({
+                    'topic': topic,
+                    'status': 'Failed',
+                    'time': 0,
+                    'error_message': e.msg if isinstance(e, WebDriverException) else str(e)
+                })
+            # Each time a topic is finished, save the log of the current process.
+            # Each topic takes ~10mins
+            with open(f'story_collection/{args.tag}_logs/logs.json', 'w', encoding='utf-8') as f:
+                json.dump(logs, f, ensure_ascii=False, indent=4)
+
+        # At the end, mark the process as finished.
+        with open(f'story_collection/{args.tag}_logs/logs.json', 'w', encoding='utf-8') as f:
+            logs[0] = 'Finished'
+            json.dump(logs, f, ensure_ascii=False, indent=4)
+
+    elif args.source == 'href':
+        topic_href = '/' + '/'.join(args.href.split('/')[-2:])
+        ret = get_all_story_per_href(driver, topic_href, args.tag)
+        print(ret)
+
+
+def get_all_story_per_href(driver: webdriver.Chrome, href: str = '/interest/example', tag: str = 'latest'):
+    '''For each of the topic, get the story informations and dump to a json file'''
+
+    # some initialization
+    root_url = 'https://ground.news'
+    topic_name = href.split('/')[-1]
+
+    # Get all story hrefs for the topic
+    story_hrefs = get_all_shref_per_topic(driver, href)
+    print('length of found story_hrefs:', len(story_hrefs))
+
+    # Get article informations
+    result = {'stats': 'finished idx: -1 / -1'}
+    for story_idx, href in enumerate(story_hrefs):
+        story_data = get_all_article_per_story(driver, root_url + href)
+        # If somehow the story contains no article, then it should not be included.
+        if len(story_data) == 0:
+            continue
         result[href.split('/')[-1].split('_')[0]] = story_data
-        # Save the stored stories every 5 discoveries
-        if (idx + 1) % 5 == 0:
-            with open(f'story_collection/interest/{href_name}.json', 'w', encoding='utf-8') as f:
-                json.dump(result, f, indent=4, ensure_ascii=False)
-    with open(f'story_collection/interest/{href_name}.json', 'w', encoding='utf-8') as f:
-        json.dump(result, f, indent=4, ensure_ascii=False)
 
+        result['stats'] = f'finished idx: {story_idx} / {len(story_hrefs)}'
+        with open(f'story_collection/{tag}_interest/{topic_name}.json', 'w', encoding='utf-8') as f:
+            json.dump(result, f, indent=4, ensure_ascii=False)
+
+    # quit driver
+    driver.quit()
+
+    # Some basic stats
     story_num = len(result)
     article_num = len([y for x in list(result.values()) for y in x])
-    return source, story_num, article_num
+
+    return href, story_num, article_num
 
 
-def get_hrefs(source: str = 'topic_list'):
+def get_all_shref_per_topic(driver: webdriver.Chrome, href: str = '/interest/example'):
+    '''For a topic, get the hrefs for stories in that topic'''
+
     root_url = "https://ground.news"
 
-    if source.startswith('/interest/'):
-        url = root_url + source
-        html_text = get_html_with_more_stories(url, more=15)
+    if href.startswith('/interest/'):
+        # Get and parse the stories displayed under that topic
+        html_text = get_html_with_more_stories(driver, root_url + href, more=15)
         soup = BeautifulSoup(html_text, 'lxml')
-        top_news = soup.find('div', class_='col-span-12 desktop:col-span-12 flex flex-col gap-3_2 desktop:pr-1_6')
-        latest_news = soup.find('div', class_='col-span-12 desktop:col-span-9 flex flex-col gap-3_2 desktop:pr-1_6')
-        hrefs_top = []
-        hrefs_latest = []
-        if top_news is not None:
-            hrefs_top = [a['href'] for a in top_news.find_all('a', href=True)]
-        if latest_news is not None:
-            hrefs_latest = [a['href'] for a in latest_news.find_all('a', href=True)]
-        hrefs = hrefs_top + hrefs_latest
 
+        # Gather hrefs
+        hrefs = []
+        for news in [
+            soup.find('div', class_='col-span-12 desktop:col-span-12 flex flex-col gap-3_2 desktop:pr-1_6'),
+            soup.find('div', class_='col-span-12 desktop:col-span-9 flex flex-col gap-3_2 desktop:pr-1_6')
+        ]:
+            if news:
+                hrefs += [a['href'] for a in news.find_all('a', href=True)]
     else:
-        raise Exception(f'the href "{source}" does not comply with the format "/interest/<topic_name>"')
+        # unknown topic href.
+        raise Exception(f'the href "{href}" does not comply with the format "/interest/<topic_name>"')
 
-    return hrefs
+    return hrefs  # This is the hrefs of the stories
 
 
-def get_one_story(full_url):
-    html_text = get_html_with_more_stories(full_url, more='all', no_login=True)
+def get_all_article_per_story(driver: webdriver.Chrome, full_url):
+    ret = []
+
+    # Get all summaries
+    html_text = get_html_with_more_stories(driver, full_url, more='all')
     soup = BeautifulSoup(html_text, 'lxml')
     summaries = soup.find_all('div', class_='relative hidden tablet:flex')
-    ret = []
+
     for idx, summary in enumerate(summaries):
         titles = summary.find_all('h4', class_='text-22 leading-11')
         names = summary.find_all('div', class_='flex bg-light-light rounded-full px-1 '
@@ -69,47 +155,44 @@ def get_one_story(full_url):
         buttons = summary.find_all('button')
         anchors = summary.find_all('a', href=True, class_='flex flex-col gap-8px cursor-pointer w-full')
         hrefs = [a['href'] for a in anchors]
-        # TODO: elegant verification
-        if len(abstracts) > 1:
-            print(titles, names, abstracts)
-        assert len(names) == 1
-        assert len(titles) == 1
-        assert len(abstracts) == 1
-        assert len(hrefs) == 1
+        assert len(names) == len(titles) == len(abstracts) == len(hrefs) == 1
+
+        # Get bias and factuality for the article.
+        bias = 'Unknown'
+        factuality = 'Unknown'
+        for b in buttons:
+            if b.text in ["Left", "Lean Left", "Center", "Lean Right", "Right"]:
+                bias = b.text
+            if b.text in ["High Factuality", "Low Factuality", "Mixed Factuality"]:
+                factuality = b.text
+
+        # Append the article into the story.
         entry = {
             'index': idx,
             'title': titles[0].text,
             'name': names[0].text,
             'abstract': abstracts[0].text,
-            'bias': buttons[0].text if len(buttons) > 0 else "Unknown",
-            'factuality': buttons[1].text if len(buttons) > 1 else "Unknown",
-            'additional badges': [button.text for button in buttons[2:]],
-            'source link': hrefs[0]
+            'bias': bias,
+            'factuality': factuality,
+            'all_badges': [button.text for button in buttons],
+            'source_link': hrefs[0]
         }
         ret.append(entry)
+
     return ret
 
 
-def get_html_with_more_stories(url, more=5, no_login=False, suppress_browser=True):
-    if suppress_browser:
-        chrome_options = Options()
-        chrome_options.add_argument("--headless")
-        driver = webdriver.Chrome(options=chrome_options)
-    else:
-        driver = webdriver.Chrome()
+def get_html_with_more_stories(driver, url, more=15):
+
     driver.get(url)
     time.sleep(1)
-    if no_login is False:
-        try:
-            login(driver)
-        except BaseException:
-            pass
-
     # Get more story id
-    button_id = 'more-stories'
     try:
+        # this is actually for loading more articles, the button has text "More Articles" on the story page
         driver.find_element(By.ID, 'more-stories')
+        button_id = 'more-stories'
     except BaseException:
+        # this is actually for loading more stories, the button has text "More Stories" on the topic page
         button_id = 'more_stories'
 
     if more == 'all':
@@ -129,7 +212,7 @@ def get_html_with_more_stories(url, more=5, no_login=False, suppress_browser=Tru
     else:
         cnt = 0
         while cnt < more:
-            time.sleep(1)
+            # time.sleep(1)
             try:
                 # close
                 close_input = driver.find_element(By.XPATH, "//button[@class='react-responsive-modal-closeButton']")
@@ -144,11 +227,10 @@ def get_html_with_more_stories(url, more=5, no_login=False, suppress_browser=Tru
             cnt += 1
 
     html_text = driver.page_source
-    driver.quit()
     return html_text
 
 
-def login(driver: webdriver.Chrome):
+def login(driver: webdriver.Chrome, username: str, password: str):
     login_button = driver.find_element(By.ID, "header-login")
     login_button.click()
 
@@ -165,69 +247,17 @@ def login(driver: webdriver.Chrome):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--rank', type=int, default=1,
-                        help='the local rank of the current process')
-    parser.add_argument('--source', type=str, choices=['href', 'topic_list'],
-                        default='href',
+    # parser.add_argument('--start_idx', type=int, default=0,
+    #                     help='the local rank of the current process')
+    # parser.add_argument('--end_idx', type=int, default=-1,
+    #                     help='the local rank of the current process')
+    parser.add_argument('--source', type=str, choices=['href', 'topic_list'], default='href',
                         help='the source of input')
-    parser.add_argument('--href', type=str,
-                        default='/interest/gun-control',
+    parser.add_argument('--href', type=str, default='/interest/gun-control',
                         help='the href to use if source is "href"')
-
+    parser.add_argument('--tag', type=str, default='latest',
+                        help='the tag to use for data version labeling')
+    parser.add_argument('--headless', action='store_true',
+                        help='to use headless mode (leave out if debugging)')
     args = parser.parse_args()
-    rank = args.rank
-
-    # Credentials
-    with open('credentials.json', 'r', encoding='utf-8') as f:
-        creds = json.load(f)
-    username = creds['username']
-    password = creds['password']
-
-    if args.source == 'topic_list':
-        # Initialize the logs of the current process.
-        logs = ['In Progress']
-        if not os.path.exits('story_collection/logs/'):
-            os.mkdir('story_collection/logs/')
-
-        with open(f'story_collection/logs/rank_{rank}.json', 'w', encoding='utf-8') as f:
-            json.dump(logs, f, ensure_ascii=False, indent=4)
-
-        # Get topic_list and preprocessing to get the segment the current process is responsible for.
-        with open('topic_collection/topic_list.json', 'r') as f:
-            topic_list = json.load(f)
-        topic_list = list(sorted(topic_list.items()))
-        SEGMENT_WIDTH = 20
-        start = rank * SEGMENT_WIDTH
-        end = min(len(topic_list), start + SEGMENT_WIDTH)
-
-        # Go through the segment and log each the status of each topic.
-        for idx, (name, href) in enumerate(topic_list[start:end]):
-            log = {'name': name}
-            try:
-                tic = time.time()
-                source, story_num, article_num = main(href)
-                toc = time.time()
-                log['status'] = 'Successful'
-                log['time'] = toc - tic
-                log['source'] = source
-                log['story_num'] = story_num
-                log['article_num'] = article_num
-                logs.append(log)
-            except (BaseException, WebDriverException) as e:
-                log['status'] = 'Failed'
-                log['time'] = 0
-                log['error message'] = e.msg if isinstance(e, WebDriverException) else str(e)
-                logs.append(log)
-            # Each time a topic is finished, save the log of the current process.
-            # Each topic takes ~10mins
-            with open(f'story_collection/logs/rank_{rank}.json', 'w', encoding='utf-8') as f:
-                json.dump(logs, f, ensure_ascii=False, indent=4)
-
-        # At the end, mark the process as finished.
-        with open(f'story_collection/logs/rank_{rank}.json', 'w', encoding='utf-8') as f:
-            logs = ['Finished'] + logs[1:]
-            json.dump(logs, f, ensure_ascii=False, indent=4)
-
-    elif args.source == 'href':
-        source, story_num, article_num = main('/' + '/'.join(args.href.split('/')[-2:]))
-        print(source, story_num, article_num)
+    main(args)
