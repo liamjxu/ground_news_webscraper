4adc9f6eb1ef579f57424fa7c6d6414a1f863233
Liam Xu <jx17@illinois.edu>
2022-10-19 00:29:52 -0500

    updated the get story scripts

--- story_collection/get_story.sh
+++ story_collection/get_story.sh
@@ -8,5 +8,12 @@
 #    python -m story_collection.main --rank $i &
 # done
 # wait
+
+# For collecting from the topic_list directly
 TAG=1016
 python -m story_collection.main --source topic_list --tag ${TAG} --headless
+
+# # For collecting with known topic (e.g., gun-violence and mass-shooting)
+# export TAG=1016
+# python -m story_collection.main --source href --href /interest/gun-violence --tag ${TAG} --headless
+# python -m story_collection.main --source href --href /interest/mass-shooting --tag ${TAG} --headless
