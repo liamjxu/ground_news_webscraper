a4e3979f45301e8d847f0638407ee864e58da24e
Liam Xu <jx17@illinois.edu>
2022-10-19 02:01:54 -0500

    update full text example

--- full_text_collection/get_full_texts.sh
+++ full_text_collection/get_full_texts.sh
@@ -10,5 +10,11 @@
 # done
 # wait
 
+# For collecting from the topic_list directly
 TAG=1016
 python -m full_text_collection.get_full_texts --source all --tag ${TAG}
+
+# # For collecting single topic (e.g., the topic of gun-violence and mass-shooting)
+# export TAG=1016 
+# python -m full_text_collection.get_full_texts --source mass-shooting --tag ${TAG}
+# python -m full_text_collection.get_full_texts --source gun-violence --tag ${TAG}
