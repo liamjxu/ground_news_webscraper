9aa4b140457ad1b027203172c91a0b23d14a8b19
Liam Xu <jx17@illinois.edu>
2023-01-15 16:20:55 -0600

    use driver manager

--- story_collection/main.py
+++ story_collection/main.py
@@ -7,6 +7,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options  # for suppressing the browser
 from selenium.common.exceptions import WebDriverException
+from webdriver_manager.chrome import ChromeDriverManager
 
 
 def main(args):
@@ -79,9 +80,9 @@ def get_all_story_per_href(args, topic_href: str = '/interest/example'):
     if args.headless:
         chrome_options = Options()
         chrome_options.add_argument("--headless")
-        driver = webdriver.Chrome(options=chrome_options)
+        driver = webdriver.Chrome(ChromeDriverManager().install(), options=chrome_options)
     else:
-        driver = webdriver.Chrome()
+        driver = webdriver.Chrome(ChromeDriverManager().install())
     driver.get('https://ground.news')
     with open('credentials.json', 'r', encoding='utf-8') as f:
         creds = json.load(f)
